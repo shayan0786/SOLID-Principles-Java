e4f37f664bda8e41c90835f5b8da39ee4699290d
Shayan Akhtar <shayan0786@users.noreply.github.com>
2026-02-03 23:07:33 +0530

    ISP Code Updated

--- Code/ISPExample.java
+++ Code/ISPExample.java
@@ -1,7 +1,37 @@
 package Code;
-
+// Good ISP here we see different interface for different use only
+interface Printable{
+    void print();
+}
+interface Scanable{
+    void scan();
+}
+interface Faxable{
+    void fax();
+}
+class SimplePrinter implements Printable {
+    public void print(){
+        System.out.println("Printing......");
+    }
+}
+class AllInOnePrinter implements Printable ,Scanable ,Faxable{
+    public void print(){
+        System.out.println("Printing...... all in one...");
+    }
+    public void scan(){
+        System.out.println("Scan....");
+    }
+    public void fax(){
+        System.out.println("Fax...");
+    }
+}
 public class ISPExample {
     public static void main(String[] args) {
-        
+        Printable p = new SimplePrinter();
+        p.print();
+        AllInOnePrinter all = new AllInOnePrinter();
+        all.print();
+        all.scan();
+        all.fax();
     }
 }
